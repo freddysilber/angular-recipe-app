582e31a17f7ec93fc88c89fe9b2c872a184cee21
freddysilber <freddy.silber@gmail.com>
2020-06-27 17:49:32 -0600

    adds recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core'
 import { Recipe } from '../recipe.model'
+import { RecipeService } from '../recipe.service'
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +9,15 @@ import { Recipe } from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>()
-  recipes: Recipe[] = [
-    new Recipe('Pizza', 'Pepperoni Pizza', 'https://www.simplyrecipes.com/wp-content/uploads/2019/09/easy-pepperoni-pizza-lead-4-768x1075.jpg'),
-    new Recipe('Pasta', 'Mac and Cheese', 'https://pinchofyum.com/wp-content/uploads/Best-Instant-Pot-Mac-and-Cheese.jpg')
-  ]
+  recipes: Recipe[]
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {
 
-  ngOnInit() { }
+  }
+
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes()
+  }
 
   onRecipeSelected(recipe: Recipe) {
     this.recipeWasSelected.emit(recipe)
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,13 @@
+import { Recipe } from './recipe.model'
+
+export class RecipeService {
+
+	private recipes: Recipe[] = [
+		new Recipe('Pizza', 'Pepperoni Pizza', 'https://www.simplyrecipes.com/wp-content/uploads/2019/09/easy-pepperoni-pizza-lead-4-768x1075.jpg'),
+		new Recipe('Pasta', 'Mac and Cheese', 'https://pinchofyum.com/wp-content/uploads/Best-Instant-Pot-Mac-and-Cheese.jpg')
+	]
+
+	public getRecipes() {
+		return this.recipes.slice()
+	}
+}
