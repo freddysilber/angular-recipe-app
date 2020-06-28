4cc927e44d44fb52edfa33f831056090cad4e80e
freddysilber <freddy.silber@gmail.com>
2020-06-27 18:36:52 -0600

    adds ingredients to shopping list feature

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,8 +1,10 @@
-import { EventEmitter } from '@angular/core'
+import { EventEmitter, Injectable } from '@angular/core'
 
 import { Recipe } from './recipe.model'
 import { Ingredient } from '../shared/ingredient.model'
+import { ShoppingListService } from '../shopping-list/shopping-list.service'
 
+@Injectable()
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>()
 
@@ -31,7 +33,15 @@ export class RecipeService {
 		)
 	]
 
+	constructor(private shoppingListService: ShoppingListService) {
+
+	}
+
 	public getRecipes() {
 		return this.recipes.slice()
+	}
+
+	public addIngredientsToShoppingList(ingredient: Ingredient[]) {
+		this.shoppingListService.addIngredients(ingredient)
 	}
 }
