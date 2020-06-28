f96556a4c9446876d5aeac01f0c1021cd767b436
freddysilber <freddy.silber@gmail.com>
2020-06-27 18:04:49 -0600

    adds shopping list service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from '@angular/core'
+
 import { Recipe } from './recipe.model'
 
 export class RecipeService {
+	recipeSelected = new EventEmitter<Recipe>()
 
 	private recipes: Recipe[] = [
 		new Recipe('Pizza', 'Pepperoni Pizza', 'https://www.simplyrecipes.com/wp-content/uploads/2019/09/easy-pepperoni-pizza-lead-4-768x1075.jpg'),
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { Ingredient } from '../shared/ingredient.model'
+
 export class ShoppingListService {
-	
+	private ingredients: Ingredient[] = [
+		new Ingredient('Apples', 10),
+		new Ingredient('Oranges', 5)
+	]
+
+	public getIngredients() {
+		return this.ingredients.slice()
+	}
+
+	// public addIngredient(){
+
+	// }
 }
