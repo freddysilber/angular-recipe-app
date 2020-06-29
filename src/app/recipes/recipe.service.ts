2b134f8b28177616c5aed2d3e0d1511323cb1624
Freddy Silber <freddy.silber@cloudcoach.com>
2020-06-29 09:39:49 -0600

    refactoring

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import { EventEmitter, Injectable } from '@angular/core'
+import { Injectable } from '@angular/core'
 
 import { Recipe } from './recipe.model'
 import { Ingredient } from '../shared/ingredient.model'
@@ -6,7 +6,6 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service'
 
 @Injectable()
 export class RecipeService {
-	recipeSelected = new EventEmitter<Recipe>()
 
 	private recipes: Recipe[] = [
 		new Recipe(
@@ -40,7 +39,6 @@ export class RecipeService {
 	}
 
 	public getRecipe(index: number) {
-		// return this.recipes.slice()[index]
 		return this.recipes[index]
 	}
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
+import { Subject } from 'rxjs'
 import { Ingredient } from '../shared/ingredient.model'
-import { EventEmitter } from '@angular/core'
 
 export class ShoppingListService {
-	ingredientsChanges = new EventEmitter<Ingredient[]>()
+	ingredientsChanges = new Subject<Ingredient[]>()
 
 	private ingredients: Ingredient[] = [
 		new Ingredient('Apples', 10),
@@ -15,11 +15,11 @@ export class ShoppingListService {
 
 	public addIngredient(ingredient: Ingredient) {
 		this.ingredients.push(ingredient)
-		this.ingredientsChanges.emit(this.ingredients.slice())
+		this.ingredientsChanges.next(this.ingredients.slice())
 	}
 
 	public addIngredients(ingredients: Ingredient[]) {
 		this.ingredients.push(...ingredients)
-		this.ingredientsChanges.emit(this.ingredients.slice())
+		this.ingredientsChanges.next(this.ingredients.slice())
 	}
 }
