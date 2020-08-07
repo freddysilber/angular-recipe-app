680b17acac68ce0c81be84829b185bb693813e7b
Freddy Silber <freddy.silber@cloudcoach.com>
2020-08-07 10:59:44 -0600

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component'
 import { RecipesResolverService } from './recipes/recipes-resolver.service'
 import { AuthComponent } from './auth/auth.component'
+import { AuthGuard } from './auth/auth.guard'
 
 const appRoutes: Routes = [
 	{
@@ -16,7 +17,10 @@ const appRoutes: Routes = [
 		pathMatch: 'full'
 	},
 	{
-		path: 'recipes', component: RecipesComponent, children: [
+		path: 'recipes',
+		component: RecipesComponent,
+		canActivate: [AuthGuard],
+		children: [
 			{
 				path: '',
 				component: RecipeStartComponent
