067f9ed4a43c40a2d2659661e8a0d3dcafa938a7
Freddy Silber <freddy.silber@cloudcoach.com>
2020-08-07 12:33:47 -0600

    dynamic components

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,28 @@
-import { Component } from '@angular/core'
+import { Component, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core'
 import { NgForm } from '@angular/forms'
 import { Router } from '@angular/router'
-import { Observable } from 'rxjs'
+import { Observable, Subscription } from 'rxjs'
 
 import { AuthService, AuthResponseData } from './auth.service'
+import { AlertComponent } from '../shared/alert/alert.component'
+import { PlaceholderDirective } from '../shared/placeholder.directive'
 
 @Component({
 	selector: 'app-auth',
 	templateUrl: './auth.component.html',
 	styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
+	@ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective
 	isLoginMode: boolean = true
 	isLoading: boolean = false
-	error: string = null
+	private closeSub: Subscription
 
-	constructor(private authService: AuthService, private router: Router) { }
+	constructor(
+		private authService: AuthService,
+		private router: Router,
+		private componentFactoryResolver: ComponentFactoryResolver
+	) { }
 
 	onSwitchMode() {
 		this.isLoginMode = !this.isLoginMode
@@ -34,12 +41,30 @@ export class AuthComponent {
 			this.toggleIsLoading()
 			this.router.navigate(['/recipes'])
 		}, errorMessage => {
-			this.error = errorMessage
+			this.showErrorAlert(errorMessage)
 			this.toggleIsLoading()
 		})
 		form.reset()
 	}
 
+	ngOnDestroy() {
+		if (this.closeSub) {
+			this.closeSub.unsubscribe()
+		}
+	}
+
+	private showErrorAlert(message: string) {
+		const component = this.componentFactoryResolver.resolveComponentFactory(AlertComponent)
+		const hostViewContainerRef = this.alertHost.viewContainerRef
+		hostViewContainerRef.clear()
+		const componentRef = hostViewContainerRef.createComponent(component)
+		componentRef.instance.message = message
+		this.closeSub = componentRef.instance.close.subscribe(() => {
+			this.closeSub.unsubscribe()
+			hostViewContainerRef.clear()
+		})
+	}
+	
 	private toggleIsLoading() {
 		this.isLoading = !this.isLoading
 	}
