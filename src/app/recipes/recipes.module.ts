e8788be4f623843f5b8f8e58937a4a607bb9cf5b
freddysilber <freddy.silber@gmail.com>
2020-08-09 11:14:21 -0600

    dropdown config

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component'
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component'
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component'
+import { RecipeListComponent } from './recipe-list/recipe-list.component'
+import { RecipesComponent } from './recipes.component'
+import { RecipeStartComponent } from './recipe-start/recipe-start.component'
+
+@NgModule({
+	declarations: [
+		RecipeDetailComponent,
+		RecipeEditComponent,
+		RecipeItemComponent,
+		RecipeListComponent,
+		RecipesComponent,
+		RecipeStartComponent
+	],
+	imports: [
+		RouterModule,
+		CommonModule,
+		ReactiveFormsModule
+	],
+	exports: [
+		RecipeDetailComponent,
+		RecipeEditComponent,
+		RecipeItemComponent,
+		RecipeListComponent,
+		RecipesComponent,
+		RecipeStartComponent
+	]
+})
+export class RecipesModule {
+
+}
--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -8,13 +8,13 @@ export class DropdownDirective {
 	// Bind to the open class on the element
 	@HostBinding('class.open') isOpen = false
 	// ! this adds dropdown closing from outside the template (aka other directive placements??)
-	// @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-	// 	this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
-	// }
-	// Toggle the open thing
-	@HostListener('click') toggleOpen() {
-		this.isOpen = !this.isOpen
+	@HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+		this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
 	}
+	// Toggle the open thing
+	// @HostListener('click') toggleOpen() {
+	// 	this.isOpen = !this.isOpen
+	// }
 
 	constructor(private elRef: ElementRef) { }
 
