1331e378f398526a8024f8078c540db5b21f2863
Freddy Silber <freddy.silber@cloudcoach.com>
2020-08-10 10:32:39 -0600

    refactoring

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -8,17 +8,3 @@ export class Recipe {
 		public ingredients: Ingredient[]
 	) { }
 }
-
-// Below is the long way to do this
-
-// export class Recipe {
-// 	public name: string
-// 	public description: string
-// 	public imagePath: string
-
-// 	constructor(name: string, description: string, imagePath: string) {
-// 		this.name = name
-// 		this.description = description
-// 		this.imagePath = imagePath
-// 	}
-// }
--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -17,10 +17,6 @@ export class RecipesResolverService implements Resolve<Recipe[]> {
 
 	resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
 		const recipes = this.recipesService.getRecipes()
-		if (recipes.length === 0) {
-			return this.dataStorageService.fetchRecipes()
-		} else {
-			return recipes
-		}
+		return recipes.length === 0 ? this.dataStorageService.fetchRecipes() : recipes
 	}
 }
